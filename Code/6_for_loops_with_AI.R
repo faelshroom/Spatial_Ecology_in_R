70cff0c8c579896568a430b636b719548b585ad9
faelshroom <ramilli.eleonora@gmail.com>
2025-11-20 17:16:08 +0100

    Update 6_for_loops_with_AI.R

--- Code/6_for_loops_with_AI.R
+++ Code/6_for_loops_with_AI.R
@@ -1,1 +1,20 @@
+#how to repeat commands in a simple way
 
+ for(sp in species_list) {
+   subdat<-kerinci[kerinci$Sps== sp,]
+   times<-subadt$Timecirc
+   densityPlot(times, main=paste("Kernel desnisty for", sp))
+ }
+species_list <- unique(kerinci$Sps)
+> n <- length(species_list)
+> par(mfrow = c(ceiling(n/2), 2))
+> for (sp in species_list) {
++     
++     subset_data <- kerinci[kerinci$Sps == sp, ]
++     t <- subset_data$circ
++     
++     densityPlot(t,
++                 main = paste("Kernel Density -", sp),
++                 xlab = "Time (radians)")
++ }
+#this way we get all the plots in a graph, to compare them
