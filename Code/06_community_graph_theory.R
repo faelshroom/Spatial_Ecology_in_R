7bd5b3a676031384be16df3663231ea8be0c6f49
faelshroom <ramilli.eleonora@gmail.com>
2025-11-20 17:24:06 +0100

    Update 06_community_graph_theory.R

--- Code/06_community_graph_theory.R
+++ Code/06_community_graph_theory.R
@@ -1,1 +1,17 @@
 #how the different species are related and the temporary overlap of the species during the days
+#confusion if it's number 8 or 6
+#Code for graph theory in ecology
+library(igraph)
+species<-c("Algae","Zooplankton","Small Fish", "Large Fish", "Bird")
+species
+"Algae"       "Zooplankton" "Small Fish"  "Large Fish"  "Bird"       
+predator<-c("Zooplankton","Small Fish", "Large Fish", "Bird")
+prey<-c("Algae","Zooplankton","Small Fish", "Large Fish")
+#now we build the dataframe, first time doing it
+interactions<- data.frame(predator,prey)
+interactions
+     predator        prey
+1 Zooplankton       Algae
+2  Small Fish Zooplankton
+3  Large Fish  Small Fish
+4        Bird  Large Fish
